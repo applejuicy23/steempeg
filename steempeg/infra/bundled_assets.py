59097cceed8496ee08711832b6465444c3777828
applejuicy23 <256155370+applejuicy23@users.noreply.github.com>
2026-09-25 23:29:13 +0300

    chore(assets): ship pause_player.png in frozen packs

--- steempeg/infra/bundled_assets.py
+++ steempeg/infra/bundled_assets.py
@@ -114,6 +114,7 @@ _BASE_ASSETS = (
     # About / easter-egg logo variant.
     "phibechipeegg.png",
     "play2.png",
+    "pause_player.png",
     "windows.png",
     "linux.png",
     "steamdeck.png",
